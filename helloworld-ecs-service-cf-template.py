b0152eac761abc48bfc6cd0a2f35e652c7108229
Junho Lee <evans8580@gmail.com>
2018-02-09 11:52:26 +0900

    Configuring logging

--- helloworld-ecs-service-cf-template.py
+++ helloworld-ecs-service-cf-template.py
@@ -1,7 +1,9 @@
 """Generating CloudFormation template."""
 from troposphere.ecs import (
   TaskDefinition,
-  ContainerDefinition
+  ContainerDefinition,
+  LogConfiguration,
+  Environment,
 )
 from troposphere import ecs
 from awacs.aws import (
@@ -50,6 +52,16 @@ t.add_resource(TaskDefinition(
       Name="helloworld",
       PortMappings=[ecs.PortMapping(
         ContainerPort=3000)]
+      Environment=[
+        Environment(Name='HELLOWORLD_VERSION', Value=Ref("Tag"))
+      ],
+      LogConfiguration=LogConfiguration(
+        LogDriver="awslogs",
+        Options={
+          'awslogs-group': "/aws/ecs/helloworld",
+          'awslogs-region': Ref("AWS::Region"),
+        }
+      ),
     )
   ],
 ))
